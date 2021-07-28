cc97358e3075b9e38133304fb2e1a9eeb82aea01
PiyushKumar-in <64825123+PiyushKumar-in@users.noreply.github.com>
2021-07-28 11:29:35 +0530

    yy

--- assets/js/piyush.js
+++ assets/js/piyush.js
@@ -1,8 +1,3 @@
 function tryos(uri){
-    var my_uri = window.location.href;
-    if(my_uri.includes("#")){
-        my_uri = my_uri.split("#")[0];
-    }
-    console.log(my_uri.replace("//","/"))
-    window.open(my_uri + uri)
+    window.open("https://piyushkumar-in.github.io/" + uri)
 }
