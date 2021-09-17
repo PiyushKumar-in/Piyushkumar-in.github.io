e50dfc080ab54c2ffc96412a80ab80c95b92e029
PiyushKumar-in <64825123+PiyushKumar-in@users.noreply.github.com>
2021-09-17 17:01:27 +0530

    Update piyush.js

--- assets/js/piyush.js
+++ assets/js/piyush.js
@@ -3,3 +3,7 @@ function tryos(uri){
     gg = gg.replace("#","")
     window.open(gg + uri)
 }
+
+function loadzip(){
+    window.open("https://drive.google.com/file/d/1XXbWqrs4gWwhWuWApdLpRgjBpvHvKlgK/view?usp=sharing")
+}
